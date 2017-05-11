ea18a84a482973d7ec198e904979cbf6ed477316
Mary Kate Fain <mkfain1@gmail.com>
2017-05-11 15:59:27 -0400

    main js

---
+++ www/main.js
@@ -0,0 +1,39 @@
+
+/*var player1 = prompt("Who is Player 1?");
+
+var player2 = prompt("Who is Player 2?");*/
+
+//Resets the board to be blank (step 1)
+function clearBoard(){
+  document.getElementById("box1").innerHTML = "";
+  document.getElementById("box2").innerHTML = "";
+  document.getElementById("box3").innerHTML = "";
+  document.getElementById("box4").innerHTML = "";
+  document.getElementById("box5").innerHTML = "";
+  document.getElementById("box6").innerHTML = "";
+  document.getElementById("box7").innerHTML = "";
+  document.getElementById("box8").innerHTML = "";
+  document.getElementById("box9").innerHTML = "";
+};
+
+
+//Game Play Function Calls
+function play(){
+  //step 1: clear the board
+  clearBoard();
+
+  //step 2: Player 1 turn 1
+
+
+
+
+//End Play function
+};
+
+
+
+
+
+/*function clearBoard () {
+  alert("test");
+}*/
