cc38a6370310be02b5537689188d07bb5e80e8bc
Mary Kate Fain <mkfain1@gmail.com>
2017-05-12 12:01:08 -0400

    place x and place O work

--- www/main.js
+++ www/main.js
@@ -17,43 +17,46 @@ function clearBoard(){
   document.getElementById("box9").innerHTML = "";
 };
 
+//_________MAKE X SQUARE_______________________________
+
 //Writes an X
-function writeHTML (id){
+function writeHTMLX (id){
   document.getElementById(id).innerHTML = "<h3>X</h3>";
 }
 
 //Necessary to make callback work, gets id attribute
-function myNamedFunction() {
+function myNamedFunctionX() {
     var id = this.getAttribute("id");
-    writeHTML(id);
-    endTurn();
+    writeHTMLX(id);
+    endTurnX();
+    placeO();
     console.log("test writeX");
 }
 
 //step 3: remove event listeners
-function endClick(id) {
-  console.log("test endClick");
+function endClickX(id) {
+  console.log("test endClickX");
 
-  document.getElementById(id).removeEventListener("click", myNamedFunction);
+  document.getElementById(id).removeEventListener("click", myNamedFunctionX);
 }
 
-function endTurn(){
-  endClick("box1");
-  endClick("box2");
-  endClick("box3");
-  endClick("box4");
-  endClick("box5");
-  endClick("box6");
-  endClick("box7");
-  endClick("box8");
-  endClick("box9");
+function endTurnX(){
+  endClickX("box1");
+  endClickX("box2");
+  endClickX("box3");
+  endClickX("box4");
+  endClickX("box5");
+  endClickX("box6");
+  endClickX("box7");
+  endClickX("box8");
+  endClickX("box9");
 };
 
 
 //step 2: Place X in box when one of the divs is clicked, and then call next turn
 
 function writeX(id) {
-  document.getElementById(id).addEventListener("click", myNamedFunction);
+  document.getElementById(id).addEventListener("click", myNamedFunctionX);
 };
 
 function placeX(){
@@ -68,8 +71,65 @@ function placeX(){
   writeX("box9");
 };
 
+//_________MAKE X SQUARE_______________________________
 
-//Game Play Function Calls
+//Writes an O
+function writeHTMLO (id){
+  document.getElementById(id).innerHTML = "<h3>O</h3>";
+}
+
+//Necessary to make callback work, gets id attribute
+function myNamedFunctionO() {
+    var id = this.getAttribute("id");
+    writeHTMLO(id);
+    endTurnO();
+    console.log("test writeO");
+}
+
+//step 3: remove event listeners
+function endClickO(id) {
+  console.log("test endClickO");
+
+  document.getElementById(id).removeEventListener("click", myNamedFunctionO);
+}
+
+function endTurnO(){
+  endClickO("box1");
+  endClickO("box2");
+  endClickO("box3");
+  endClickO("box4");
+  endClickO("box5");
+  endClickO("box6");
+  endClickO("box7");
+  endClickO("box8");
+  endClickO("box9");
+};
+
+
+//step 3: Place O in box when one of the divs is clicked, and then end turn
+
+function writeO(id) {
+  document.getElementById(id).addEventListener("click", myNamedFunctionO);
+};
+
+function placeO(){
+  writeO("box1");
+  writeO("box2");
+  writeO("box3");
+  writeO("box4");
+  writeO("box5");
+  writeO("box6");
+  writeO("box7");
+  writeO("box8");
+  writeO("box9");
+};
+
+
+
+
+
+
+//_____________GAME PLAY_______________________
 function play(){
 
   //step 1: clear the board
@@ -79,7 +139,6 @@ function play(){
   placeX();
 
   //step 3: Player 2 turn 1, and end turn
-  placeO();
 
 //End Play function
 };
