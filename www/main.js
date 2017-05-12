485040fb177df9effd225bb73ee3ce9a3644f972
Mary Kate Fain <mkfain1@gmail.com>
2017-05-12 11:54:11 -0400

    end event listener on X turn works

--- www/main.js
+++ www/main.js
@@ -1,4 +1,5 @@
 
+
 /*var player1 = prompt("Who is Player 1?");
 
 var player2 = prompt("Who is Player 2?");*/
@@ -16,13 +17,24 @@ function clearBoard(){
   document.getElementById("box9").innerHTML = "";
 };
 
+//Writes an X
+function writeHTML (id){
+  document.getElementById(id).innerHTML = "<h3>X</h3>";
+}
 
+//Necessary to make callback work, gets id attribute
+function myNamedFunction() {
+    var id = this.getAttribute("id");
+    writeHTML(id);
+    endTurn();
+    console.log("test writeX");
+}
 
 //step 3: remove event listeners
 function endClick(id) {
-  document.getElementById(id).removeEventListener("click", function() {
-    writeHTML(id);
-  });
+  console.log("test endClick");
+
+  document.getElementById(id).removeEventListener("click", myNamedFunction);
 }
 
 function endTurn(){
@@ -39,14 +51,9 @@ function endTurn(){
 
 
 //step 2: Place X in box when one of the divs is clicked, and then call next turn
-function writeHTML (id){
-  document.getElementById(id).innerHTML = "<h3>X</h3>";
-}
 
 function writeX(id) {
-  document.getElementById(id).addEventListener("click", function() {
-      writeHTML(id);
-  });
+  document.getElementById(id).addEventListener("click", myNamedFunction);
 };
 
 function placeX(){
@@ -68,11 +75,11 @@ function play(){
   //step 1: clear the board
   clearBoard();
 
-  //step 2: Player 1 turn 1
+  //step 2: Player 1 turn 1, and end turn
   placeX();
 
-  //step 3: End eventListeners from step 2
-
+  //step 3: Player 2 turn 1, and end turn
+  placeO();
 
 //End Play function
 };
